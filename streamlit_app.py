2b607661fa45806ab761020de0907f4e2633834e
JCandiaE <124767767+JCandiaE@users.noreply.github.com>
2023-02-13 15:12:40 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -65,7 +65,7 @@ try:
 # output it the screen as a table
 # streamlit.dataframe(fruityvice_normalized)
 
-import snowflake.connector
+#import snowflake.connector
 #import URLerror
 
 #don't run anything past here while we troubleshoot
